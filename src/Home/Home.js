e00528223a63de6849ab3cd606d35805b071e022
Jon Smith <jjsmith.okc@gmail.com>
2023-11-19 17:47:10 -0600

    Add styling

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
+        <div className='home-container'>
             <h1>Welcome to Jon Smith Codes!</h1>
             <p>This is the homepage of my coding accomplishments.</p>
-            <img src="/profile.png" alt="Profile image" />"
+            <img src="/profile.png" alt="Profile image" />
         </div>
     );
 };
